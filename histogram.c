521050c9a653de9c99462b80e97d7a7a0a6eebb0
Augusto Amaral <augustocamaral2003@gmail.com>
2020-11-03 16:00:30 -0300

    added histogram.c file

---
+++ histogram.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+//histogram.c
+//outputs a histogram showing the lenght of the imput words
+
+int main(void) {
+    int c, l = 0;
+    int histogram[100] = {};
+    
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\n' || c == '\t') {
+            ++histogram[l];
+            l = 0;
+        } else {
+            ++l;
+        }
+    }
+
+    for (int i = 0; i < 100; i++) {
+        if (histogram[i]) {
+            printf("%-2d | ", i);
+            while(histogram[i]--) {
+                printf("#");
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
