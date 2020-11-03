c8a833e669210ca0d7a0f00d2ac93f3a573d2a67
Augusto Amaral <augustocamaral2003@gmail.com>
2020-11-03 16:01:36 -0300

    added histogram_char.c

---
+++ histogram_char.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+//histogram_char.c
+//outputs a histogram showing the occurence of chars in input
+
+int main(void) {
+    int c;
+    int histogram[95] = {};
+    
+    while ((c = getchar()) != EOF) {
+        histogram[c - ' ']++;
+    }
+
+    for (int i = 0; i < 95; i++) {
+        if (histogram[i]) {
+            printf("[%c] ", i + ' ');
+            while(histogram[i]--) {
+                printf("#");
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
