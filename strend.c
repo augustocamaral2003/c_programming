00c1965a76f3094fe3daa62021dabd090a9343f7
Augusto Amaral <augustocamaral2003@gmail.com>
2020-11-09 22:41:26 -0300

    add strend.c

---
+++ strend.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int strend(char *a, char *b) {
+    char* pos = b;
+    while (*a++ != '\0')
+        ;
+    while (*b++ != '\0')
+        ;
+    while (b != pos)
+        if (*(--b) == *(--a))
+            continue;
+        else
+            return 0;
+    return 1;
+}
+
+int main(void) {
+    char a[10] = "string";
+    char b[10] = "ring";
+    char c[10] = "yng";
+
+    printf("a = %s, b = %s, c = %s\n", a, b, c);
+    printf("strend(a, b) = %d, strend(a, c) = %d\n", strend(a, b), strend(a, c));
+    printf("strend(b, a) = %d\n", strend(b, a));
+
+    return 0;
+}
